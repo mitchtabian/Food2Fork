e11a69803d2a7ff4ea37711cdb4e0722b14c2b7c
mitch <mitch@tabian.ca>
2020-11-26 15:18:37 -0800

    update

--- Food2Fork/settings.py
+++ Food2Fork/settings.py
@@ -186,7 +186,6 @@ else:
 
     #STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
     STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
-    TEMP = os.path.join(BASE_DIR, 'temp')
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
--- TODO.md
+++ TODO.md
@@ -1,8 +1,5 @@
 # TODO
-1. Get the data on the website
-1. Rest api
-	1. Setup token auth but use a single token for now. In the future I might want to limit this.
-	
+1. Make all images public...?
 
 
 # Prod
