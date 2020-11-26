bc3c54bb6434327946d1e307c140e3e03d1976ba
mitch <mitch@tabian.ca>
2020-11-26 11:52:41 -0800

    fix allowed hosts

--- Food2Fork/settings.py
+++ Food2Fork/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["food2fork.ca", "www.food2fork.ca"]
 
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # During development only
