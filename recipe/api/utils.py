fd384587119a59e3ac085ceaa4622e570cc19e10
mitch <mitch@tabian.ca>
2020-11-27 09:30:18 -0800

    remove script

--- recipe/api/utils.py
+++ recipe/api/utils.py
@@ -1,25 +1,3 @@
-from recipe.models import Recipe
-
-
-def fix():
-	for recipe in Recipe.objects.all():
-		ingredients = []
-		for i in recipe.ingredients:
-			ingredients.append(i)
-			# print(str(i))
-		recipe.ingredient_list = ingredients
-		recipe.save()
-
-
-
-def fix2():
-	for recipe in Recipe.objects.all():
-		ingredients = []
-		for i in recipe.ingredient_list:
-			ingredients.append(i)
-			# print(str(i))
-		recipe.ingredients = ingredients
-		recipe.save() 
 
 
 
